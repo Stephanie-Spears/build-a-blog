2f10a0f3da81f17724f606b893f6a2f51de457f0
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-07-14 00:18:18 -0700

    update user functions

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
-import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-# app.secret_key = 'abc123'
+app.secret_key = 'abc123'
 
+
+#to render changes to css etc. in real time, instead of relying on possibly old, outdated caches
 """ Inspired by http://flask.pocoo.org/snippets/40/ """
 @app.url_defaults
 def hashed_url_for_static_file(endpoint, values):
@@ -49,60 +51,50 @@ class Blog(db.Model):
 @app.route('/blog', methods=["POST", "GET"])
 def blog():
     # if request.method == "POST":
-    #     blog_title = request.form['title']
-    #     blog_body = request.form['body']
-    #     new_blog = Blog(title, body)
-    #     db.session.add(new_blog)
-    #     db.session.commit()
-    # if request.method == "POST":
-    #     blog_id = request.form['blog-id']
-    #     return redirect("/?blog=" + blog_id)
-    #  if request.method == "GET":
-    #      title = request.form['title']
-    #      body = request.form['body']
-    #      return render_template('/blog', title=title
-    # if request.method == "POST":
-    #      blog_id = int(request.form['blog-id'])
-    #      blog = Blog.query.get(blog_id)
-    #      return render_template('/blog.html', blog=blog)
-        #  show_blog = Blog.query.filter_by().all()
-    #     return redirect("/?blog=" + blog_id)
-    if request.method == "POST":
-        title = request.form['title']
-        body = request.form['body']
-        return render_template('blog.html', title=title, body=body)
+    #     title = request.form['title']
+    #     body = request.form['body']
+    #     return render_template('blog.html', title=title, body=body)
+
+    if request.args.get('id'):
+        post_id = request.args.get('id')
+        post = Blog.query.filter_by(id=post_id).first()
+        return render_template('blog.html', post=post)
 
     blogs = Blog.query.all()
     return render_template('blog.html', blogs=blogs)
 
+
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
+    error = None
+
     if request.method == "POST":
         blog_title = request.form['title']
         blog_body = request.form['body']
-        new_blog = Blog(blog_title, blog_body)
-        db.session.add(new_blog)
-        db.session.commit()
-        return redirect('/')
+        blog_id = request.form['blog_id']
 
-    blogs = Blog.query.all()
+        if (not blog_title) and (not blog_body):
+            error = "empty_blog_error"
+            return render_template('newpost.html', error=error, title=blog_title, body=blog_body)
+        elif (not blog_body):
+            error = "body_error"
+            return render_template('newpost.html', error=error, title=blog_title, body=blog_body)
+        elif (not blog_title):
+            error = "title_error"
+            return render_template('newpost.html', error=error, title=blog_title, body=blog_body)
 
-    return render_template('newpost.html')
-    # return redirect('/')
+        elif error == None:
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+            return redirect("/blog?id=" + str(new_blog.id))
+            # return render_template('blog.html', blog=new_blog)
+    return render_template('newpost.html', error=error)
 
-# @app.route('/newpost', methods=['POST', 'GET'])
-# def new_post():
-    # after submitting, redirects to main Blog page
-    #if blog title or blog body is left empty, the form is rendered again with a helpful error message, and it retains the previously entered inputs
-    # return redirect('/')
 
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
-
-    blogs = Blog.query.all()
-
-    return render_template('blog.html', title="Build A Blog", blogs=blogs)
+    return redirect('/blog')
 
 if __name__ == "__main__":
     app.run()
